5acb0c518568b0f5bf9fa60efdcabab8e90ae291
cviejo <1179357+cviejo@users.noreply.github.com>
2025-01-09 16:32:52 +0000

    added event to onset detection

---
+++ examples/onset-detector.ck
@@ -0,0 +1,28 @@
+@import "OnsetDetector.chug";
+
+SndBuf buffer("./examples/1.wav");
+
+buffer => OnsetDetector onset => blackhole;
+
+fun void loop() {
+	while (buffer.samples()::samp => now) {
+		buffer.pos(0);
+		buffer.play(1);
+	}
+}
+spork ~loop();
+
+// detect using the event
+fun void event() {
+	while (onset.event => now) {
+		<<< "onset event" >>>;
+	}
+}
+spork ~event();
+
+// detect using the sample
+while (1::samp => now) {
+	if (onset.last() != 0.0) {
+		<<< "onset sample" >>>;
+	}
+}
--- src/onset-detector/OnsetDetector.cpp
+++ src/onset-detector/OnsetDetector.cpp
@@ -1,17 +1,20 @@
 #include "chugin.h"
-
 #include "rings/dsp/onset_detector.h"
 
 CK_DLL_CTOR(onsetdetector_ctor);
 CK_DLL_DTOR(onsetdetector_dtor);
 CK_DLL_TICKF(onsetdetector_tick);
-
 CK_DLL_MFUN(onsetdetector_setParams);
 
-t_CKINT dataOffset = 0;
+t_CKINT offsetEvent = 0;
+t_CKINT offsetData = 0;
+
+static CBufferSimple* eventBuffer = NULL;
 
 struct OnsetDetectorData {
 	rings::OnsetDetector detector;
+	Chuck_Event* event;
+	Chuck_VM* vm;
 };
 
 CK_DLL_QUERY(OnsetDetector) {
@@ -31,7 +34,8 @@ CK_DLL_QUERY(OnsetDetector) {
 	QUERY->add_arg(QUERY, "float", "decimatedSR");
 	QUERY->add_arg(QUERY, "float", "ioiTime");
 
-	dataOffset = QUERY->add_mvar(QUERY, "int", "@od_data", false);
+	offsetEvent = QUERY->add_mvar(QUERY, "Event", "event", false);
+	offsetData = QUERY->add_mvar(QUERY, "int", "@data", false);
 
 	QUERY->end_class(QUERY);
 
@@ -39,28 +43,38 @@ CK_DLL_QUERY(OnsetDetector) {
 }
 
 CK_DLL_CTOR(onsetdetector_ctor) {
-	OBJ_MEMBER_INT(SELF, dataOffset) = 0;
+	if (eventBuffer == NULL) {
+		eventBuffer = eventBuffer = API->vm->create_event_buffer(VM);
+	}
 
 	OnsetDetectorData* data = new OnsetDetectorData;
-
 	float srate = API->vm->srate(VM);
-
 	data->detector.Init(8.0f / srate, 160.0f / srate, 1600.0f / srate, srate, 0.005f);
+	data->vm = VM;
+	OBJ_MEMBER_INT(SELF, offsetData) = (t_CKINT)data;
 
-	OBJ_MEMBER_INT(SELF, dataOffset) = (t_CKINT)data;
+	Chuck_Type* eventType = API->type->lookup(VM, "Event");
+	Chuck_Object* event = API->object->create(SHRED, eventType, true);
+	data->event = (Chuck_Event*)event;
+	OBJ_MEMBER_INT(SELF, offsetEvent) = (t_CKINT)event;
 }
 
 CK_DLL_DTOR(onsetdetector_dtor) {
-	OnsetDetectorData* data = (OnsetDetectorData*)OBJ_MEMBER_INT(SELF, dataOffset);
+	OnsetDetectorData* data = (OnsetDetectorData*)OBJ_MEMBER_INT(SELF, offsetData);
 
 	if (data) {
 		delete data;
-		OBJ_MEMBER_INT(SELF, dataOffset) = 0;
+		OBJ_MEMBER_INT(SELF, offsetData) = 0;
 	}
+
+	// TODO: cleanup buffer and event
+	// can't figure out how to do this properly via chugin.h,
+	// destroy_event_buffer is not there and CK_SAFE_ADD_REF is not working.
+	// buffer cleanup is not that critical since it's only created once.
 }
 
 CK_DLL_MFUN(onsetdetector_setParams) {
-	OnsetDetectorData* data = (OnsetDetectorData*)OBJ_MEMBER_INT(SELF, dataOffset);
+	OnsetDetectorData* data = (OnsetDetectorData*)OBJ_MEMBER_INT(SELF, offsetData);
 
 	float low = GET_NEXT_FLOAT(ARGS);
 	float lowMid = GET_NEXT_FLOAT(ARGS);
@@ -72,12 +86,18 @@ CK_DLL_MFUN(onsetdetector_setParams) {
 }
 
 CK_DLL_TICKF(onsetdetector_tick) {
-	OnsetDetectorData* data = (OnsetDetectorData*)OBJ_MEMBER_INT(SELF, dataOffset);
+	OnsetDetectorData* data = (OnsetDetectorData*)OBJ_MEMBER_INT(SELF, offsetData);
 
 	bool onsetDetected = data->detector.Process(in, nframes);
 
+	if (onsetDetected && data->event) {
+		API->vm->queue_event(data->vm, data->event, 1, eventBuffer);
+	}
+
+	float value = onsetDetected ? 1.0f : 0.0f;
+
 	for (int i = 0; i < nframes; i++) {
-		out[i] = onsetDetected ? 1.0f : 0.0f;
+		out[i] = value;
 	}
 
 	return TRUE;
